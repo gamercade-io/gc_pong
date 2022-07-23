19113645dd9992f44e2169180491e39e856eb88a
RobDavenport <zerve0@hotmail.com>
2022-07-23 17:28:38 +0900

    update to new version

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@ const SCREEN_HEIGHT: i32 = 180;
 struct GameState {
     paddles: [Paddle; 2],
     ball: Ball,
+    ball_color: i32,
+    paddle_color: [i32; 2],
 }
 
 struct Paddle {
@@ -38,7 +40,7 @@ struct Ball {
 
 impl Ball {
     const RADIUS: i32 = 4;
-    const SPEED: f32 = 0.66;
+    const SPEED: f32 = 0.75;
 
     const fn new() -> Self {
         Self {
@@ -56,33 +58,22 @@ static mut GAME_STATE: GameState = GameState {
         Paddle::new((SCREEN_WIDTH - (10 + Paddle::WIDTH)) as f32),
     ],
     ball: Ball::new(),
+    ball_color: 0,
+    paddle_color: [0, 0],
 };
 
 #[no_mangle]
-pub unsafe extern "C" fn init() {}
+pub unsafe extern "C" fn init() {
+    GAME_STATE.ball_color = color_index(43);
+    GAME_STATE.paddle_color[0] = color_index(31);
+    GAME_STATE.paddle_color[1] = color_index(15);
+}
 
 #[no_mangle]
 pub unsafe extern "C" fn update() {
     let ball = &mut GAME_STATE.ball;
     let player_1 = &mut GAME_STATE.paddles[0];
     let player_2 = &mut GAME_STATE.paddles[1];
-
-    // Handle inputs
-    if button_up_held(0) == 1 {
-        player_1.y -= Paddle::SPEED;
-    }
-
-    if button_down_held(0) == 1 {
-        player_1.y += Paddle::SPEED;
-    }
-
-    if button_up_held(1) == 1 {
-        player_2.y -= Paddle::SPEED;
-    }
-
-    if button_down_held(1) == 1 {
-        player_2.y += Paddle::SPEED;
-    }
 
     // Physics Simulation
     ball.x += ball.x_vel;
@@ -97,7 +88,7 @@ pub unsafe extern "C" fn update() {
         *ball = Ball::new();
     }
 
-    if (ball.y as i32 - Ball::RADIUS) < 0 || (ball.y as i32 + Ball::RADIUS > SCREEN_HEIGHT) {
+    if (ball.y as i32 - (Ball::RADIUS + 1)) < 0 || (ball.y as i32 + Ball::RADIUS > SCREEN_HEIGHT - 1) {
         ball.y_vel *= -1.0;
     }
 
@@ -108,6 +99,33 @@ pub unsafe extern "C" fn update() {
     } else {
         if intersects(ball, player_2) {
             ball.x_vel *= -1.0;
+        }
+    }
+
+    // Handle inputs
+    if button_up_held(0) == 1 {
+        player_1.y -= Paddle::SPEED;
+    }
+
+    if button_down_held(0) == 1 {
+        player_1.y += Paddle::SPEED;
+    }
+
+    if num_players() == 2 {
+        // Handle Player Input
+        if button_up_held(1) == 1 {
+            player_2.y -= Paddle::SPEED;
+        }
+
+        if button_down_held(1) == 1 {
+            player_2.y += Paddle::SPEED;
+        }
+    } else {
+        // Be an AI
+        if player_2.y > ball.y {
+            player_2.y -= Paddle::SPEED
+        } else if (player_2.y + Paddle::HEIGHT as f32) < ball.y {
+            player_2.y += Paddle::SPEED
         }
     }
 
@@ -136,24 +154,27 @@ pub unsafe extern "C" fn draw() {
     let player_1 = &GAME_STATE.paddles[0];
     let player_2 = &GAME_STATE.paddles[1];
 
-    clear_screen(0, 0);
+    clear_screen(0);
 
-    circle(ball.x as i32, ball.y as i32, Ball::RADIUS, 43, 0);
+    circle(
+        GAME_STATE.ball_color,
+        ball.x as i32,
+        ball.y as i32,
+        Ball::RADIUS,
+    );
 
     rect_filled(
+        GAME_STATE.paddle_color[0],
         player_1.x as i32,
         player_1.y as i32,
         Paddle::WIDTH,
         Paddle::HEIGHT,
-        31,
-        0,
     );
     rect_filled(
+        GAME_STATE.paddle_color[1],
         player_2.x as i32,
         player_2.y as i32,
         Paddle::WIDTH,
         Paddle::HEIGHT,
-        15,
-        0,
     );
 }
